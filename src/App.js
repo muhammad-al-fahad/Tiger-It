812305a05b9b83d3b4135707afd3dae0ffb99d69
Zohaib <dev.zohaibmahota@gmail.com>
2023-11-23 11:06:11 +0500

    Five auth Form done

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <>
       <Navbar />
       {/* <SignIn /> */}
-      <ForgetPassword />
+      {/* <ForgetPassword /> */}
       {/* <FirstName /> */}
       {/* <Personalinfo /> */}
       <ConfirmPass />
