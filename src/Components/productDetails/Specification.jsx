48ae4fc958c7361bc0ae48514c54c3d5760611d7
Qulbe12 <qulbehussain@outlook.com>
2023-11-26 18:14:32 +0500

    new component of product detail has been made on route of product-details

---
+++ src/Components/productDetails/Specification.jsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import {Box, Flex, Text} from "@mantine/core";
+
+const Specification = () => {
+    return (
+        <Flex justify="space-between" w="100%">
+            <Flex bg="white" w="30%" direction="column" gap={12} py="lg">
+                <Box>
+                    <Text color="#767C8C" fw={400}>Expandable Memory Type</Text>
+                    <Text fw={600}>No Expandable Memory</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>SIM Type</Text>
+                    <Text fw={600}>Nano + ESIM</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Internal Memory</Text>
+                    <Text fw={600}>256 GB</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Primary Camera Resolution</Text>
+                    <Text fw={600}>48 MP</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Fast Charging</Text>
+                    <Text fw={600}>yes</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Camera Type</Text>
+                    <Text fw={600}>Primary Camera + Secondary Camera</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Audio Jack</Text>
+                    <Text fw={600}>Lightning Port</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Model Year</Text>
+                    <Text fw={600}>2023</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Model Number</Text>
+                    <Text fw={600}>MQ9U3AA/A / MQ9A3AH/A</Text>
+                </Box>
+            </Flex>
+            <Flex bg="white" w="30%" direction="column" gap={12} py="lg">
+                <Box>
+                    <Text color="#767C8C" fw={400}>Secondary Camera Resolution</Text>
+                    <Text fw={600}>12 MP</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>SIM Count</Text>
+                    <Text fw={600}>Dual SIM</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Ram Size</Text>
+                    <Text fw={600}>6 GB</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Version</Text>
+                    <Text fw={600}>Middle East Version</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Colour Name</Text>
+                    <Text fw={600}>Space Black</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Processor Name</Text>
+                    <Text fw={600}>Apple</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Primary Camera Feature</Text>
+                    <Text fw={600}>Triple</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Battery Type</Text>
+                    <Text fw={600}>Lithium Ion</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Network Type</Text>
+                    <Text fw={600}>5G</Text>
+                </Box>
+            </Flex>
+            <Flex bg="white" w="30%" direction="column" gap={12} py="lg">
+                <Box>
+                    <Text color="#767C8C" fw={400}>Expandable Memory Type</Text>
+                    <Text fw={600}>No Expandable Memory</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>SIM Type</Text>
+                    <Text fw={600}>Nano + ESIM</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Internal Memory</Text>
+                    <Text fw={600}>256 GB</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Primary Camera Resolution</Text>
+                    <Text fw={600}>48 MP</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Fast Charging</Text>
+                    <Text fw={600}>yes</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Camera Type</Text>
+                    <Text fw={600}>Primary Camera + Secondary Camera</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Audio Jack</Text>
+                    <Text fw={600}>Lightning Port</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Model Year</Text>
+                    <Text fw={600}>2023</Text>
+                </Box>
+                <Box>
+                    <Text color="#767C8C" fw={400}>Model Number</Text>
+                    <Text fw={600}>MQ9U3AA/A / MQ9A3AH/A</Text>
+                </Box>
+            </Flex>
+        </Flex>
+    );
+};
+
+export default Specification;
